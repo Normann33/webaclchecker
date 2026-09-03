cc4c514ea69586e549231664225b3c352a2c14e2
normann <s.kotelnikov@rts.ru>
2026-09-03 12:15:47 +0300

    Changes to be committed: 	modified:   webaclchecker.py

--- webaclchecker.py
+++ webaclchecker.py
@@ -69,7 +69,7 @@ def run(username, password, prot, src, dst, dst_port, gw, vrf):
             password=password,
             secret=enable,
             device_type='cisco_ios',
-            port='2222'
+            port='22'
         ) as connector:
             
             version = Version(connector)
